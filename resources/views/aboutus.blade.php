3c6991878f80d214b000ed6d7e442be59fae8628
Inkotanyi <sebakire@gmail.com>
2024-12-03 15:46:30 +0200

    adding commit

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -59,15 +59,6 @@
     <script src="assets/dist/js/revolution-slider/js/jquery.themepunch.tools.min.js"></script>
     <script src="assets/dist/js/revolution-slider/js/jquery.themepunch.revolution.min.js"></script>
 
-    <!-- <link href="css/style.css" rel="stylesheet" type="text/css"> -->
-
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
- <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
- <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-<![endif]-->
 </head>
 
 <body class="tm-enable-navbar-scrolltofixed tm-enable-navbar-always-visible-on-scroll">
@@ -115,12 +106,14 @@
 
                                                     </li>
 
-                                                    <li><a href="{{ route('causes') }}">Causes</a>
+                                                    <li><a href="#Causes">Causes</a>
                                                     </li>
-                                                    <li><a href="{{ route('events') }}">Events</a>
+                                                    <li><a href="#contact">Contact-Us</a>
                                                     </li>
-                                                    <li><a href="{{ route('galery') }}">Galery</a></li>
-                                                    <li><a href="#">Donate</a></li>
+
+                                                    <li><a href="#Parteners">Parteners</a>
+                                                    </li>
+
                                                 </ul>
                                                 </li>
 
@@ -1019,7 +1012,7 @@
             </div>
         </section>
 
-        <div class="section-title text-center mb-40">
+        <div class="section-title text-center mb-40" id="Parteners">
             <div class="row justify-content-md-center">
                 <div class="col-md-6">
                     <h2 class="title text-uppercase mt-0"><span class="">Our</span> <span
@@ -1061,7 +1054,7 @@
                 </div>
             </div>
         </section>
-    </div> --}}
+    </div>
     <!-- end main-content -->
 
     <!-- Footer -->
