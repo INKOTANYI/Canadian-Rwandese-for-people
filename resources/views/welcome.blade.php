c2b5a7aa7ca6f75fe791de335ab6377049aea6f3
Inkotanyi <sebakire@gmail.com>
2024-12-16 04:01:39 +0200

    changing image on hmpage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -447,11 +447,11 @@
 
                                         <li data-index="rs-1" data-transition="slidingoverlayhorizontal"
                                             data-slotamount="default" data-easein="default" data-easeout="default"
-                                            data-masterspeed="default" data-thumb="images/bg/rwandahills (9).jpeg"
+                                            data-masterspeed="default" data-thumb="images/bg/rwandahills (6).jpeg"
                                             data-rotate="0" data-fstransition="fade" data-fsmasterspeed="1500"
                                             data-fsslotamount="7" data-saveperformance="off" data-title="Slide One">
                                             <!-- MAIN IMAGE -->
-                                            <img src="images/bg/rwandahills (9).jpeg" alt=""
+                                            <img src="images/bg/rwandahills (6).jpeg" alt=""
                                                 data-bgposition="center 20%" data-bgfit="cover"
                                                 data-bgrepeat="no-repeat" data-bgparallax="10" class="rev-slidebg"
                                                 data-no-retina>
